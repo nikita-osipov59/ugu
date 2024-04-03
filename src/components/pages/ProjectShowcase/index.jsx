4b0542c826a4c8fdd4f34f17324432a99e8c6608
MaksWork <sauageog@gmail.com>
2024-04-03 19:21:46 +0300

    модульный стиль на главную страницу и фильтр как ui

--- src/components/pages/ProjectShowcase/index.jsx
+++ src/components/pages/ProjectShowcase/index.jsx
@@ -1,57 +1,51 @@
+import { Button } from '../../ui/Button';
+import { Filter } from '../../ui/Filter';
 import { NavBar } from '../../ui/Navbar';
-import './ProjectShowcase.scss'
+import style from './ProjectShowcase.module.scss';
 
 const ProjectShowcase = () => {
     return(
         <div>
             <NavBar/>
             
-            <div className='body_cont'>
-                 <div className='filter_cont'> {/*//TODO перенести фильтр в отдельный файл и добавить чекбоксы */}
-                    <h2>Фильтры</h2>
-                    <p>Руководитель</p>
-                    <p>Год набора</p>
-                    <p>Тип заказчика</p>
-                    <p>Вид проекта</p>
-                    <p>Направление идентичности ЮГУ</p>
-                    <p>Сфера проекта</p>
-                </div>
-                <div className='search_cont'>
+            <div className={style.body_cont}>
+                <Filter/>
+                <div className={style.search_cont}>
                     <input placeholder='Название проекта'/>
-                    <button>Найти</button>
+                    <Button title={'Найти'} color={'white'} background={'#61FF00'}/>
                 </div>
-                <div className='project_list_cont'>
-                    <div className='project_cont'>
+                <div className={style.project_list_cont}>
+                    <div className={style.project_cont}>
                         <h2>Производство деталей с использованием аддитивных технологий (3D печать)</h2>
                         <p>Срок до: 15.05.2024</p>
                         <p>Мест: 6</p>
                         <p>Год набора 2022</p>
                     </div>
-                    <div className='project_cont'>
+                    <div className={style.project_cont}>
                         <h2>Производство деталей с использованием аддитивных технологий (3D печать)</h2>
                         <p>Срок до: 15.05.2024</p>
                         <p>Мест: 6</p>
                         <p>Год набора 2022</p>
                     </div>
-                    <div className='project_cont'>
+                    <div className={style.project_cont}>
                         <h2>Производство деталей с использованием аддитивных технологий (3D печать)</h2>
                         <p>Срок до: 15.05.2024</p>
                         <p>Мест: 6</p>
                         <p>Год набора 2022</p>
                     </div>
-                    <div className='project_cont'>
+                    <div className={style.project_cont}>
                         <h2>Производство деталей с использованием аддитивных технологий (3D печать)</h2>
                         <p>Срок до: 15.05.2024</p>
                         <p>Мест: 6</p>
                         <p>Год набора 2022</p>
                     </div>
-                    <div className='project_cont'>
+                    <div className={style.project_cont}>
                         <h2>Производство деталей с использованием аддитивных технологий (3D печать)</h2>
                         <p>Срок до: 15.05.2024</p>
                         <p>Мест: 6</p>
                         <p>Год набора 2022</p>
                     </div>
-                    <div className='project_cont'>
+                    <div className={style.project_cont}>
                         <h2>Производство деталей с использованием аддитивных технологий (3D печать)</h2>
                         <p>Срок до: 15.05.2024</p>
                         <p>Мест: 6</p>
@@ -60,7 +54,7 @@ const ProjectShowcase = () => {
                 
                 </div>
             </div>
-            <div className='project_page_switcher'> {/* //TODO когда поднимут бек сделать постраничное распределение проектов */}
+            <div className={style.project_page_switcher}> {/* //TODO когда поднимут бек сделать постраничное распределение проектов */}
                 <button>{"<"}</button>
                 <button>{">"}</button>
             </div>
