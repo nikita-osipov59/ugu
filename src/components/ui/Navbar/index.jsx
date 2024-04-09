c1e25f95cee552beb81aecc3fc9fed4eaa891863
MaksWork <sauageog@gmail.com>
2024-04-10 01:25:07 +0300

    подключил бек в навбаре

--- src/components/ui/Navbar/index.jsx
+++ src/components/ui/Navbar/index.jsx
@@ -1,19 +1,41 @@
 import { UserContext } from '../../UserProvider';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { ROUTER_PATH } from '../../const/PATH';
 import './index.scss';
 
 export const NavBar = () => {
+    let [isMenu, setMenu] = useState(false);
+
+    const showHideMenu = () =>{
+        const menu = document.getElementsByClassName('nav_menu')[0];
+        if(isMenu){
+            menu.style.display = 'none'
+            setMenu(false)
+        }
+        else{
+            menu.style.display = 'flex'
+            setMenu(true)
+        }
+    }
+
+    const quit = () =>{
+        localStorage.clear()
+        navigate(ROUTER_PATH.AUTH)
+    }
+    
     const { user } = useContext(UserContext);
+    const navigate = useNavigate();
     
     return(
         <div>
             <ul className='navbar_cont'>
                 <li><img className='logo_cont'/></li>
-                <li><button>{user.login}</button></li>
-                <div>
-                    <button>Главная</button>
-                    <button>Профиль</button>
-                    <button>Выход</button>
+                <li><button onClick={showHideMenu}>{user.login}</button></li>
+                <div className='nav_menu'>
+                    <button onClick={() => navigate(ROUTER_PATH.HOME)}>Главная</button>
+                    <button>Профиль</button> {/*//TODO в зависимости от роли в тот профиль и кидать юзера */}
+                    <button onClick={quit}>Выход</button> {/*TODO сделать выход думаю через setAuth(false) и перенаправление на страницу регистрации*/}
                 </div>
             </ul>
         </div>
