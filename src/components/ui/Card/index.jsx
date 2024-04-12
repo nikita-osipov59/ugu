38ecff775c2fe46274754bba45e12f8d0bd3470d
nikita-osipov59 <67689788+nikita-osipov59@users.noreply.github.com>
2024-04-13 00:37:01 +0300

    card component

--- src/components/ui/Card/index.jsx
+++ src/components/ui/Card/index.jsx
@@ -2,11 +2,18 @@ import React from "react";
 
 import Style from "./Style.module.scss";
 
-export const Card = ({ name, description }) => {
+export const Card = ({ name, description, info }) => {
   return (
     <div className={Style.card}>
       <h1 className={Style.cardTitle}>{name}</h1>
       <div className={Style.cardDescription}>{description}</div>
+      {info ? (
+        <ul>
+          <li>Срок до:</li>
+          <li>Мест:</li>
+          <li>Год набора:</li>
+        </ul>
+      ) : null}
     </div>
   );
 };
