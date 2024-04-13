80e2edb0511c117170b456e5403f6b4ddacfe6cc
nikita-osipov59 <67689788+nikita-osipov59@users.noreply.github.com>
2024-04-13 16:34:11 +0300

    Card component, and other fix

--- src/api/functions.js
+++ src/api/functions.js
@@ -1,72 +1,72 @@
 import axios from "axios";
 
 export const getOrdersAll = async (user) => {
-    const response = await axios
-        .get("https://backendyogy.onrender.com/api/v1/orders/all", {
-            headers: {
-                Authorization: `Bearer ${user.access_token}`,
-            },
-        })
-        .then(({ data }) => {
-            let res = data;
-            return res;
-        })
-        .catch(function (error) {
-            console.log(error);
-    });
-    return response
-};
-
-export const getCurrentMember = async (user) =>{
-    const response = await axios
-      .get("https://backendyogy.onrender.com/api/v1/auth/", {
-        headers: {
-          Authorization: `Bearer ${user.access_token}`,
-        },
-      })
-      .then(({ data }) => {
-        const res = data;
-        return res;
-      })
-      .catch(function (error) {
-        console.log(error);
-    });
-    return response
-}
-
-export const getSortedOrders = async (query, user) => {
-    const respons = await axios
-      .get(
-        `https://backendyogy.onrender.com/api/v1/orders/sorted?mode=${query}&offset=0`,
-        {
-          headers: {
-            Authorization: `Bearer ${user.access_token}`,
-          },
-        }
-      )
-      .then(({ data }) => {
-        const res = data;
-        return res;
-      })
-      .catch(function (error) {
-        console.log(error);
-    });
-    return respons;
-}
-
-export const getProjectsAll = async (user) =>{
   const response = await axios
-    .get("https://backendyogy.onrender.com/api/v1/projects/get/all",{
+    .get("https://backendyogy.onrender.com/api/v1/orders/all", {
       headers: {
         Authorization: `Bearer ${user.access_token}`,
       },
     })
-    .then(({data}) =>{
+    .then(({ data }) => {
+      let res = data;
+      return res;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return response;
+};
+
+export const getCurrentMember = async (user) => {
+  const response = await axios
+    .get("https://backendyogy.onrender.com/api/v1/user/", {
+      headers: {
+        Authorization: `Bearer ${user.access_token}`,
+      },
+    })
+    .then(({ data }) => {
       const res = data;
-      return res
+      return res;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return response;
+};
+
+export const getSortedOrders = async (query, user) => {
+  const response = await axios
+    .get(
+      `https://backendyogy.onrender.com/api/v1/orders/sorted?mode=${query}&offset=0`,
+      {
+        headers: {
+          Authorization: `Bearer ${user.access_token}`,
+        },
+      }
+    )
+    .then(({ data }) => {
+      const res = data;
+      return res;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return response;
+};
+
+export const getProjectsAll = async (user) => {
+  const response = await axios
+    .get("https://backendyogy.onrender.com/api/v1/projects/get/all", {
+      headers: {
+        Authorization: `Bearer ${user.access_token}`,
+      },
+    })
+    .then(({ data }) => {
+      const res = data;
+      return res;
     })
     .catch((error) => {
       console.log(error);
-    })
-    return response;
-}
+    });
+  return response;
+};
