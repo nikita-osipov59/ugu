89f7d9f5ab2fc31b32adbc593693b8fa7e9dd82a
MaksWork <sauageog@gmail.com>
2024-04-15 15:23:50 +0300

    рендер писем проекта и начало создания писем

--- src/api/functions.js
+++ src/api/functions.js
@@ -91,7 +91,6 @@ export const getProjectsAll = async (user) => {
 export const getProjectById = async (user, id) => {
   const response = await axios
     .get(`https://backendyogy.onrender.com/api/v1/projects/get/${id}`, {
-      //TODO когда будет гет на определенный проект поменять ссылку
       headers: {
         Authorization: `Bearer ${user.access_token}`,
       },
@@ -105,3 +104,37 @@ export const getProjectById = async (user, id) => {
     });
   return response;
 };
+
+export const getProjectLetters = async (user, project_id) =>{
+  const response = await axios
+    .get(`https://backendyogy.onrender.com/api/v1/letter/all/${project_id}`, {
+      headers: {
+        Authorization: `Bearer ${user.access_token}`,
+      },
+    })
+    .then(({ data }) => {
+      let res = data;
+      return res;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return response;
+} 
+
+export const addNewLetter = async (user, project_id, text) =>{
+  const response = await axios
+    .post(`https://backendyogy.onrender.com/api/v1/letter/add${project_id}`, {
+      headers: {
+        Authorization: `Bearer ${user.access_token}`,
+      },
+      text,
+    })
+    .then((res) =>{
+      console.log(res);
+    })
+    .catch((error) =>{
+      console.log(error);
+    })
+    return response;
+}
